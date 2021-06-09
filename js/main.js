fca44340e0b4ef39844bece169fa71ac5d17b787
ander.uraga@gmail.com <ander.uraga@gmail.com>
2021-06-09 13:56:54 +0200

    conseguir precio MKR

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+const el_pi = document.getElementById('pi');
+const el_pf = document.getElementById('pf');
+
 function calc(){
 
-    let el_pi = document.getElementById('pi');
-    let el_pf = document.getElementById('pf');
+    
+    
     let el_dolar = document.getElementById('dolar');
     let el_cantidad = document.getElementById('cantidad');
     let el_fee = document.getElementById('fee');
@@ -14,3 +17,19 @@ function calc(){
     p_dolar.innerHTML =  diferencia + ' $';
     p_euro.innerHTML = ( diferencia * el_dolar.value ) +' €';
 }
+
+
+const MAKER = "MKRBUSD";
+const API_V3_PRICE = "https://api.binance.com/api/v3/ticker/price?symbol=";
+
+
+fetch(API_V3_PRICE + MAKER)
+  .then(response => response.json())
+  .then(data => { 
+      console.log(data);
+      el_pi.value = data.price;
+      el_pf.value = data.price;
+  })
+  .catch(function(error) {
+    console.log('Hubo un problema con la petición Fetch:' + error.message);
+  });
